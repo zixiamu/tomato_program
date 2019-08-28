9c3ecdafff4471f92630545486ca8fe860d75deb
zixiamu <1214145161@qq.com>
2019-08-28 13:01:53 +0800

    封装http

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,4 +1,5 @@
-// pages/login/login.js
+const {http}=require('../../lib/http.js')
+const {app_id,app_secret}=getApp().globalData
 Page({
 
   /**
@@ -7,60 +8,33 @@ Page({
   data: {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  //点击按钮 调用小程序的wx.login  参数  http.post 返回user 保存user 返回首页
+  login(e){
+    let encrypted_data = e.detail.encryptedData
+    let iv = e.detail.iv
+    this.wxLogin(encrypted_data,iv)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  wxLogin(encrypted_data,iv){
+    wx.login({
+      success:(res)=> {
+        this.loginMe(res.code, iv, encrypted_data)
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  loginMe(code, iv, encrypted_data){
+    http.post("/sign_in/mini_program_user", { 
+      code, 
+      iv, 
+      encrypted_data, 
+      app_id, 
+      app_secret 
+      })
+      .then(response => {
+      wx.setStorageSync('me', response.data.resource)
+      wx.setStorageSync('X-token', response.header["X-token"])
+      wx.reLaunch({
+        url: '/pages/home/home',
+      })
+    })
   }
 })
