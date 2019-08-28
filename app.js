005a4769c0f029a3e9c711cc019d92d205e215e4
zixiamu <1214145161@qq.com>
2019-08-28 13:11:35 +0800

    去除app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-App({
-  globalData: {
-    host: 'https://gp-server.hunger-valley.com',  //照抄
-    app_id: "wx67a30da4e4b54631",
-    app_secret: "fbb119aeca854622ef65627c7d1b3e9d",
-    t_app_id: "tZUCQgejjB5xHZEZ1mGdhsQ8",
-    t_app_secret: "2i33fa1VHCXnzhpab5Suvnta"
-  }
-})
